91967330fa22b4bde3c4a2f0b1cc06765ef76780
Lorenzo Ferrante <lorenzoferrante96@gmail.com>
2025-07-29 17:15:01 +0200

    Initial Commit

---
+++ Nexus/NexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusApp.swift
+//  Nexus
+//
+//  Created by Lorenzo Ferrante on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct NexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
